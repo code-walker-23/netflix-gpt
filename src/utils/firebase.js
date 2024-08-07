67d0a72a43f53e31e864c419ed4e720d911cfb1c
code-walker-23 <chhabravinay549@gmail.com>
2024-08-07 15:32:01 +0530

    Changes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,26 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-  authDomain: "netflix-gpt-watch-tv-shows.firebaseapp.com",
-  projectId: "netflix-gpt-watch-tv-shows",
-  storageBucket: "netflix-gpt-watch-tv-shows.appspot.com",
-  messagingSenderId: "266002146761",
-  appId: "1:266002146761:web:44ac232fa266e70988f418",
-  measurementId: "G-H0MV1836LS",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth();
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
