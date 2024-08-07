049696eb5dd57a81db201ab5bc1702a6798af15d
code-walker-23 <chhabravinay549@gmail.com>
2024-08-07 15:19:27 +0530

    Changes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const db = getFirestore(app);
 export default app;
