0cbc3d1a6ddb352871142a7e5b39ea44e1552e35
code-walker-23 <chhabravinay549@gmail.com>
2024-08-27 20:28:14 +0530

    updated

--- src/components/ActorDetail/CreditDetail.js
+++ src/components/ActorDetail/CreditDetail.js
@@ -19,7 +19,7 @@ const CreditDetail = ({ creditDetail }) => {
             />
           </div>
 
-          {/* Media and Actor Information */}
+        
           <div className="flex-1">
             <h3 className="text-3xl font-bold text-white mb-2">{media.title}</h3>
             <p className="text-sm text-gray-400 mb-2">
